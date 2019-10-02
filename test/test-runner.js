3721ec744bf2429d2bca460a5a844e76f88be17d
James Holton <james_holton@yahoo.co.uk>
2019-10-02 14:24:33 +0100

    Bit of reformatting to make me happy

--- test/test-runner.js
+++ test/test-runner.js
@@ -1,8 +1,14 @@
 // contains test instructions
 var test_note = new note('JS rocks');
-function testDefaultText() { return arguments.callee.name + ': ' + assert.isTrue(test_note.returnText() === 'JS rocks') }
-
-
 var test_note_array =  new noteList()
 test_note_array.createNote("Favorite drink: Beer")
-function testNewNoteOnList() { return arguments.callee.name + ': ' + assert.isTrue(test_note_array.returnList().pop().returnText() === 'Favorite drink: Beer') }
+
+function testDefaultText() { 
+  result = assert.isTrue(test_note.returnText() === 'JS rocks');
+  return arguments.callee.name + ': ' + result;
+};
+
+function testNewNoteOnList() { 
+  result = assert.isTrue(test_note_array.returnList().pop().returnText() === 'Favorite drink: Beer');
+  return arguments.callee.name + ': ' + result;
+};
